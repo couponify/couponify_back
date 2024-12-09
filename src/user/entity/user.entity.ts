9700bb8896e44d087610b37bb8c91010461f42d4
ljyo2o9 <yggosxoljy@gmail.com>
2024-12-09 14:00:52 +0900

    feat(user.entity.ts): 사용자 엔티티 추가 사용자 정보를 저장하기 위한 User 엔티티를 생성하여 데이터베이스와의 상호작용을 가능하게 함.

---
+++ src/user/entity/user.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryColumn, Column } from 'typeorm';
+
+@Entity('user')
+export class User {
+  @PrimaryColumn({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  nickname: string;
+
+  @Column()
+  imageUrl: string;
+}
