f0bd0050dd4c0846fe8e6810fb3df00a1b4254eb
ljyo2o9 <yggosxoljy@gmail.com>
2024-12-12 16:24:33 +0900

    ✨ (auth.controller.ts): 회원가입 기능을 위한 POST 엔드포인트 추가 💡 요청 DTO 및 파일 업로드 인터셉터를 사용하여 이미지 처리

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,27 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignUpRequestDto } from './dto/request/signup-request.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  @HttpCode(HttpStatus.CREATED)
+  @UseInterceptors(FileInterceptor('image'))
+  async signup(
+    @Body() signupRequest: SignUpRequestDto,
+    @UploadedFile() image: Express.Multer.File,
+  ) {
+    return await this.authService.signup(signupRequest, image);
+  }
 }
