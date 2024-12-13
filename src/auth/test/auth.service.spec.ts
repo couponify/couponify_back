39d7dc845b3292aeb8df868e166bd9c678c938e8
ljyo2o9 <yggosxoljy@gmail.com>
2024-12-13 15:20:18 +0900

    ✅ (auth.service.spec.ts): AuthService의 로그인 및 토큰 생성 테스트 추가 💡 테스트 케이스를 추가하여 로그인 및 토큰 생성 로직의 정확성을 검증하기 위함.

--- src/auth/test/auth.service.spec.ts
+++ src/auth/test/auth.service.spec.ts
@@ -6,11 +6,15 @@ import { User } from 'src/user/entity/user.entity';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { UserAlreadyExistsException } from 'src/exception/custom/user-already-exists.exception';
+import { JwtService } from '@nestjs/jwt';
+import { UserNotFoundException } from 'src/exception/custom/user-not-found.exception';
+import { UserPasswordNotMatchedException } from 'src/exception/custom/user-password-not-matched.exception';
 
 describe('AuthService', () => {
   let authService: AuthService;
   let configService: ConfigService;
   let userRepository: Repository<User>;
+  let jestService: JwtService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -30,18 +34,57 @@ describe('AuthService', () => {
             get: jest.fn(),
           },
         },
+        {
+          provide: JwtService,
+          useValue: {
+            signAsync: jest.fn(),
+          },
+        },
       ],
     }).compile();
 
     authService = module.get<AuthService>(AuthService);
     userRepository = module.get<Repository<User>>(getRepositoryToken(User));
     configService = module.get<ConfigService>(ConfigService);
+    jestService = module.get<JwtService>(JwtService);
   });
 
   it('should Defined', () => {
     expect(AuthService).toBeDefined();
     expect(userRepository).toBeDefined();
     expect(configService).toBeDefined();
+  });
+
+  describe('tokenGenerate', () => {
+    it('should return a token', async () => {
+      const user = {
+        email: 'test@gmail.com',
+      } as User;
+
+      jest.spyOn(jestService, 'signAsync').mockResolvedValue('token');
+
+      const result = await authService.tokenGenerate(user, false);
+      const result2 = await authService.tokenGenerate(user, true);
+
+      expect(result).toEqual('token');
+      expect(result2).toEqual('token');
+      expect(jestService.signAsync).toHaveBeenNthCalledWith(
+        1,
+        { email: user.email, isRefreshToken: false },
+        {
+          secret: configService.get('JWT_SECRET'),
+          expiresIn: configService.get('JWT_EXPIRATION'),
+        },
+      );
+      expect(jestService.signAsync).toHaveBeenNthCalledWith(
+        2,
+        { email: user.email, isRefreshToken: true },
+        {
+          secret: configService.get('JWT_RE_SECRET'),
+          expiresIn: configService.get('JWT_RE_EXPIRATION'),
+        },
+      );
+    });
   });
 
   describe('signup', () => {
@@ -124,6 +167,77 @@ describe('AuthService', () => {
       const result = authService.signup(signUpRequest, image);
 
       await expect(result).rejects.toThrow(UserAlreadyExistsException);
+    });
+  });
+
+  describe('login', () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+      jest.clearAllTimers();
+    });
+
+    it('should return a token', async () => {
+      const loginRequest = {
+        email: 'test@gmail.com',
+        password: 'test',
+      };
+
+      const user = {
+        email: 'test@gmail.com',
+        password: 'hashedPassword',
+      } as User;
+
+      jest.spyOn(userRepository, 'findOneBy').mockResolvedValue(user);
+      jest.spyOn(bcrypt, 'compare').mockImplementation((a, b) => true);
+      jest.spyOn(authService, 'tokenGenerate').mockResolvedValue('token');
+
+      const result = await authService.login(loginRequest);
+      expect(result).toEqual({
+        accessToken: 'token',
+        refreshToken: 'token',
+      });
+
+      expect(userRepository.findOneBy).toHaveBeenCalledWith({
+        email: loginRequest.email,
+      });
+      expect(bcrypt.compare).toHaveBeenCalledWith(
+        loginRequest.password,
+        user.password,
+      );
+      expect(authService.tokenGenerate).toHaveBeenCalledWith(user, false);
+      expect(authService.tokenGenerate).toHaveBeenCalledWith(user, true);
+    });
+
+    it('should throw an error if user not found', async () => {
+      const loginRequest = {
+        email: 'test@gmail.com',
+        password: 'test',
+      };
+
+      jest.spyOn(userRepository, 'findOneBy').mockResolvedValue(null);
+
+      const result = authService.login(loginRequest);
+
+      await expect(result).rejects.toThrow(UserNotFoundException);
+    });
+
+    it('should throw an error if password not match', async () => {
+      const loginRequest = {
+        email: 'test@gmail.com',
+        password: 'test',
+      };
+
+      const user = {
+        email: 'test@gmail.com',
+        password: 'hashedPassword',
+      } as User;
+
+      jest.spyOn(userRepository, 'findOneBy').mockResolvedValue(user);
+      jest.spyOn(bcrypt, 'compare').mockImplementation((a, b) => false);
+
+      const result = authService.login(loginRequest);
+
+      await expect(result).rejects.toThrow(UserPasswordNotMatchedException);
     });
   });
 });
