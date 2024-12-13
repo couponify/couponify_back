c9541490d9145378a7eb49a4739e8446cb12113f
ljyo2o9 <yggosxoljy@gmail.com>
2024-12-13 09:03:16 +0900

    ✨ (auth.service.ts): 로그인 기능 및 JWT 토큰 생성 로직 추가 🔧 (auth.service.ts): 사용자 인증 관련 예외 처리 개선

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,9 +6,12 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/user/entity/user.entity';
 import { SignUpRequestDto } from './dto/request/signup-request.dto';
 import { SignUpResponseDto } from './dto/response/signup-response.dto';
-import { CustomException } from 'src/exception/custom.exception';
 import { UserAlreadyExistsException } from 'src/exception/custom/user-already-exists.exception';
-
+import { LoginRequestDto } from './dto/request/login-request.dto';
+import { UserNotFoundException } from 'src/exception/custom/user-not-found.exception';
+import { UserPasswordNotMatchedException } from 'src/exception/custom/user-password-not-matched.exception';
+import { JwtService } from '@nestjs/jwt';
+import { LoginResponseDto } from './dto/response/login-response.dto';
 
 @Injectable()
 export class AuthService {
@@ -16,7 +19,22 @@ export class AuthService {
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
     private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
   ) {}
+
+  async tokenGenerate(user: User, isRefreshToken: boolean) {
+    return await this.jwtService.signAsync(
+      { email: user.email, isRefreshToken },
+      {
+        secret: isRefreshToken
+          ? this.configService.get('JWT_RE_SECRET')
+          : this.configService.get('JWT_SECRET'),
+        expiresIn: isRefreshToken
+          ? this.configService.get('JWT_RE_EXPIRATION')
+          : this.configService.get('JWT_EXPIRATION'),
+      },
+    );
+  }
 
   async signup(
     signupRequest: SignUpRequestDto,
@@ -42,5 +60,24 @@ export class AuthService {
     });
 
     return new SignUpResponseDto(await this.userRepository.save(user));
+  }
+
+  async login(loginRequest: LoginRequestDto) {
+    const { email, password } = loginRequest;
+
+    const user = await this.userRepository.findOneBy({ email });
+    if (!user) {
+      throw new UserNotFoundException();
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new UserPasswordNotMatchedException();
+    }
+
+    return new LoginResponseDto(
+      await this.tokenGenerate(user, false),
+      await this.tokenGenerate(user, true),
+    );
   }
 }
